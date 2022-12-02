aef7192a5bd06ecd0a42e3848348cd8eac975bf3
mujtabaaliawan <117171332+mujtabaaliawan@users.noreply.github.com>
2022-12-02 16:49:41 +0500

    b

--- scheduler.py
+++ scheduler.py
@@ -5,9 +5,9 @@ import os
 
 def main():
     print('Scheduler initialised')
-    schedule.every(1).minutes.do(lambda: os.system('scrapy crawl stock'))
-    schedule.every(2).minutes.do(lambda: os.system('python file_reader.py'))
-    print('Next job is set to run at: ' + str(schedule.next_run()))
+    schedule.every(5).minutes.do(lambda: os.system('scrapy crawl stock'))
+    schedule.every(330).seconds.do(lambda: os.system('python file_reader.py'))
+    print('Next spider is set to run at: ' + str(schedule.next_run()))
 
     while True:
         schedule.run_pending()
