c41462a0657eaac62f5251d856a01a46fae1c920
mujtabaaliawan <117171332+mujtabaaliawan@users.noreply.github.com>
2022-12-12 16:04:55 +0500

    Update graph_requester.py

--- graph_requester.py
+++ graph_requester.py
@@ -4,19 +4,9 @@ import requests
 
 def main():
 
-    data = {
-        "company_id": 1,
-        "from_datetime": "2022-12-06 13:05:25",
-        "to_datetime": "2022-12-08 18:50:25"
-    }
-
-    json_data = json.dumps(data)
-
-    URL = "http://127.0.0.1:8000/graph"
-    header = {'Content-type': 'application/json'}
-    response = requests.post(url=URL, data=json_data, headers=header)
-    graph_data = json.loads(response.content)
-    print(graph_data)
+    URL = "http://127.0.0.1:8000/graph?company=1&date_time_lt=2022-12-06 13:05:25&date_time_gt=2022-12-13 18:50:25"
+    response = requests.get(url=URL)
+    print(response.content)
 
 
 if __name__ == '__main__':
