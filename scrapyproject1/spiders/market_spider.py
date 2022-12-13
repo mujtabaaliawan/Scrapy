9358c65bbca8dc9ff609aedae58ad28d94f72470
mujtabaaliawan <117171332+mujtabaaliawan@users.noreply.github.com>
2022-12-13 16:14:55 +0500

    spider extraction updated

--- scrapyproject1/spiders/market_spider.py
+++ scrapyproject1/spiders/market_spider.py
@@ -14,24 +14,24 @@ class StockSpider(scrapy.Spider):
         market_date = response.css("div.col-sm-12.inner-content-table")
         market_last_updated = market_date.css("h4::text").get()
 
-        for table in response.css("div.table-responsive"):
+        market_main_board = response.css("#marketmainboard")
+        for table in market_main_board.css("div.table-responsive"):
 
             category_name = table.css("h4::text").extract()
-            company_data = table.css("td.dataportal::text").extract()
-            price_data = table.css("td.dataportal ~ td::text").extract()
-            i = 0
+            company_data = table.css("tr td.dataportal")
+
             for company in company_data:
+                price_data = company.css("td.dataportal ~ td::text").extract()
                 product = ItemLoader(item=StockItem())
                 product.add_value('category', category_name)
-                product.add_value('company', company)
-                product.add_value('ldcp', price_data[i])
-                product.add_value('open', price_data[i+1])
-                product.add_value('high', price_data[i+2])
-                product.add_value('low', price_data[i+3])
-                product.add_value('current', price_data[i+4])
-                product.add_value('change', price_data[i+6])
-                product.add_value('volume', price_data[i+7])
-                i = i + 8
+                product.add_value('company', company.css("::text").extract())
+                product.add_value('ldcp', price_data[0])
+                product.add_value('open', price_data[1])
+                product.add_value('high', price_data[2])
+                product.add_value('low', price_data[3])
+                product.add_value('current', price_data[4])
+                product.add_value('change', price_data[6])
+                product.add_value('volume', price_data[7])
                 product.add_value('date_time', market_last_updated)
                 yield product.load_item()
 
